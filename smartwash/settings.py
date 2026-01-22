2c477f9849f2ca3e6b7f0e598b1c9a078645b154
Priyagokul2418 <priyagokul854@gmail.com>
2026-01-22 11:19:47 +0530

    updated

--- smartwash/settings.py
+++ smartwash/settings.py
@@ -87,17 +87,19 @@ TEMPLATES = [
 # =========================
 # DATABASE (MySQL)
 # =========================
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": os.getenv("DB_NAME", "laundry"),
-#         "USER": os.getenv("DB_USER", "smartwash_user"),
-#         "PASSWORD": os.getenv("DB_PASSWORD", "smartwash_user"),
-#         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-#         "PORT": os.getenv("DB_PORT", "3306"),
-#         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
-#     }
-# }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME", "laundry"),
+        "USER": os.getenv("DB_USER", "smartwash_user"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "smartwash_user"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+    }
+}
 
 
 
